a20b61d62ce63e5ebbe1bdee9d9bba245d225133
Ivaylo2017 <ivoaleksiev@yahoo.com>
2018-04-09 20:53:52 -0400

    All old deleted

--- src/main/java/com/fastsusu/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.fastsusu.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer 
-						extends AbstractSecurityWebApplicationInitializer {
-
-}
